906b2862cad182394bd8715b953fe5c5eb63fb21
Eva Larumbe <evalarumbe@gmail.com>
2020-05-31 20:11:55 +0200

    Add a library that's only used before bundling. Doesn't even make it out to the dist

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('I log therefore I am');
+import library from './some-library.js';
+
+(() => {
+  const main = function () {
+    console.log(`I log therefore I am chilling at ${library.name}`);
+  };
+
+  window.addEventListener('load', main);
+})();
---
+++ src/some-library.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'Shakespeare & Co',
+};
+
+// Named export is also chill:
+// export const library = {
+//   name: 'Shakespeare & Bro',
+// };
