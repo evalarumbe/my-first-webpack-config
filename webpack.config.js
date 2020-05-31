dc76b9f55b94a0c5c3b783a1a66ba1fdcb8055c0
Eva Larumbe <evalarumbe@gmail.com>
2020-06-01 00:13:21 +0200

    Add config options: babel loader, dev mode, context, source map (is source map working?)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map', // TODO: why doesn't this seem to change anything? source mapping is sweet so far without it
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './src/index.js',
+    main: './index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
   },
   output: {
     filename: '[name].bundle.js',
