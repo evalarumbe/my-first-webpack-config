b8784fcf4d8ed382ce13df8d98b85bcae90297e6
Eva Larumbe <evalarumbe@gmail.com>
2020-06-01 23:01:02 +0200

    Install Babel with @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
   entry: {
     main: './src/index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
