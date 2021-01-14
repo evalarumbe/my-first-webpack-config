a9fb1f6748c9f97bb8272f1f88eb76b2e57f2b64
Eva Larumbe <evalarumbe@gmail.com>
2021-01-14 14:11:01 +0100

    Babel: fix core-js dependency, set useBuiltIns: usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// Babel polyfills
-import 'core-js/stable'; // FIXME? On each build, Babel debug text (set it to true to see) says "Import of core-js was not found" (the error comes from other src files. I didn't think I had to import this for every file, just at the entry point like right here. What's going on? So far I'm not even using any core-js polyfills so haven't properly tested). NOTE: If I import these 2 lines at the top of each js file, the warning stops (Maybe only the corejs line is relevant but the docs seem to recommend them together).
-import 'regenerator-runtime/runtime';
-
 // local scripts
 import { renderPosts, setTheme } from './js/ui.js';
 
